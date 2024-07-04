ee69623a087806e26f8d8916e32c1cbd04447bc8
mosheze1311 <mosheze1311@gmail.com>
2024-07-04 11:55:00 +0300

    added a function to calculate A, b at once.

--- poisson_blending.py
+++ poisson_blending.py
@@ -58,6 +58,34 @@ def create_b_vector(mask, target, source, center, num_roi_pixels, mask_indices, 
     return b
 
 
+def create_A_matrix_and_b_vector(mask, target, source, center, num_roi_pixels, mask_indices, index_map):
+    h, w = mask.shape
+    A = lil_matrix((num_roi_pixels, num_roi_pixels))
+    b = np.zeros((num_roi_pixels, 3))
+    center_col, center_row = center
+
+    for index, flat_coordinate in enumerate(mask_indices):
+        row, col = divmod(flat_coordinate, w)
+
+        A[index, index] = -4
+        b[index] = -4 * source[row, col]
+
+        for (d_row, d_col) in FOUR_DIR:
+            neighbor_row, neighbor_col = row + d_row, col + d_col
+            if 0 <= neighbor_row < h and 0 <= neighbor_col < w:
+                b[index] += source[neighbor_row, neighbor_col]
+
+            neighbor_flat_coordinate = (neighbor_row * w + neighbor_col)
+            if neighbor_flat_coordinate in mask_indices:
+                neighbor_index = index_map[neighbor_flat_coordinate]
+                A[index, neighbor_index] = 1
+            else:
+                target_row = (neighbor_row - h // 2) + center_row
+                target_col = (neighbor_col - w // 2) + center_col
+                b[index] -= target[target_row, target_col]
+    return csr_matrix(A), b
+
+
 def blend_images(source, target, mask, center):
     mask = mask // 255
 
@@ -66,20 +94,16 @@ def blend_images(source, target, mask, center):
     # indexing pixel position in mask with natural numbers' order
     index_map = {linear_index: idx for idx, linear_index in enumerate(mask_indices)}
 
-    A = construct_A_matrix(mask, num_roi_pixels, mask_indices, index_map)
+    # A = construct_A_matrix(mask, num_roi_pixels, mask_indices, index_map)
+    # b = create_b_vector(mask, target, source, center, num_roi_pixels, mask_indices, index_map)
 
-    b = create_b_vector(mask, target, source, center, num_roi_pixels, mask_indices, index_map)
-    b = b
-    X = spsolve(A, b)
+    A, b = create_A_matrix_and_b_vector(mask, target, source, center, num_roi_pixels, mask_indices, index_map)
+    X = spsolve(A, b).clip(0, 255)
 
-    rows, cols = mask.shape
-    X = X.reshape((-1, 3)).clip(0,255)
-
-    rows, cols, channels = target.shape
     result = target.copy()
+
     rows, cols = mask.shape
     target_rows, target_cols, channels = target.shape
-
     for index, linear_index in enumerate(mask_indices):
         row, col = divmod(linear_index, cols)
         target_row = center[1] + row - rows // 2
@@ -93,9 +117,9 @@ def blend_images(source, target, mask, center):
 
 def parse():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--src_path', type=str, default='./data/imgs/banana1.jpg', help='image file path')
-    parser.add_argument('--mask_path', type=str, default='./data/seg_GT/banana1.bmp', help='mask file path')
-    parser.add_argument('--tgt_path', type=str, default='./data/bg/table.jpg', help='mask file path')
+    parser.add_argument('--src_path', type=str, default='./data/imgs/fullmoon.jpg', help='image file path')
+    parser.add_argument('--mask_path', type=str, default='./data/seg_GT/fullmoon.bmp', help='mask file path')
+    parser.add_argument('--tgt_path', type=str, default='./data/bg/wall.jpg', help='mask file path')
     return parser.parse_args()
 
 
