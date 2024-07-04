0da4f1dbccd171ef1584a11d57d49da77b326d36
danielbarlev2 <danielbl2010@gmail.com>
2024-07-04 18:57:21 +0300

    summery test edits

--- summary_tests.py
+++ summary_tests.py
@@ -36,54 +36,90 @@ if __name__ == "__main__":
     boundary_box_test_images = []
 
     poisson_test_images = (
-        ("grass_mountains.jpeg", [("bush", ), "llama", "sheep", "grave", ]),
+        ("grass_mountains.jpeg", ["bush", "llama", "sheep", "grave", ]),
         ("table.jpg", ["banana1", "banana2", "book", "teddy"]),
         ("wall.jpg", ["flower", "fullmoon", "memorial", "stone2", "cross"])
     )
+    poisson_test_images_bad_mask = (
+        ("grass_mountains.jpeg", ["sheep"]),
+        ("table.jpg", ["banana1"]),
+    )
 
-    # blur test
-    for img_name in blur_test_images:
-        img_path = f'data/imgs/{img_name}.jpg'
+    # # blur test
+    # for img_name in blur_test_images:
+    #     img_path = f'data/imgs/{img_name}.jpg'
+    #
+    #     img = cv2.imread(img_path)
+    #     cv2.imshow("no blur", img)
+    #     cv2.imshow("hard blur", blur_image(img, hard_blur_kernel))
+    #     cv2.imshow("light blur", blur_image(img, light_blur_kernel))
+    #     # cv2.waitKey(0)
+    #
+    # # components test
+    # for img_name in components_test_images:
+    #     img_path = f'data/imgs/{img_name}.jpg'
+    #     rect_path = f'data/bboxes/{img_name}.txt'
+    #
+    #     img = cv2.imread(img_path)
+    #     rect = tuple(map(int, open(rect_path, "r").read().split(' ')))
+    #     for components_count in (2, 5, 10):
+    #         grabcut(img=img, rect=rect, gmm_components=components_count)
+    #
+    # # boundary box test
+    # for img_name in boundary_box_test_images:
+    #     img_path = f'data/imgs/{img_name}.jpg'
+    #     rect_path = f'data/bboxes/{img_name}.txt'
+    #
+    #     img = cv2.imread(img_path)
+    #     rect = tuple(map(int, open(rect_path, "r").read().split(' ')))
+    #     for components_count in (2, 5, 10):
+    #         grabcut(img=img, rect=rect, gmm_components=components_count)
+    #
+    # # poisson test tight mask
+    # for tgt_img_name, src_images in poisson_test_images:
+    #     tgt_img_path = f"data/bg/{tgt_img_name}"
+    #     im_tgt = cv2.imread(tgt_img_path, cv2.IMREAD_COLOR)
+    #
+    #     center_col = int(im_tgt.shape[1] / 2)
+    #     center_row = int(im_tgt.shape[0] / 2)
+    #     center = (center_col, center_row)
+    #     for img_name in src_images:
+    #         im_src = cv2.imread(f"data/imgs/{img_name}.jpg", cv2.IMREAD_COLOR)
+    #         im_mask = cv2.imread(f"data/seg_GT/{img_name}.bmp", cv2.IMREAD_GRAYSCALE)
+    #         im_mask = cv2.threshold(im_mask, 0, 255, cv2.THRESH_BINARY)[1]
+    #         im_res = blend_images(source=im_src, target=im_tgt, center=center, mask=im_mask)
+    #
+    #         print(f"saving {img_name}")
+    #         save_path = f"data/results/PoissonBlend/{img_name}_in_{tgt_img_name.split('.')[0]}.png"
+    #         cv2.imwrite(save_path, im_res)
+    #         input()
 
-        img = cv2.imread(img_path)
-        cv2.imshow("no blur", img)
-        cv2.imshow("hard blur", blur_image(img, hard_blur_kernel))
-        cv2.imshow("light blur", blur_image(img, light_blur_kernel))
-        # cv2.waitKey(0)
-
-    # components test
-    for img_name in components_test_images:
-        img_path = f'data/imgs/{img_name}.jpg'
-        rect_path = f'data/bboxes/{img_name}.txt'
-
-        img = cv2.imread(img_path)
-        rect = tuple(map(int, open(rect_path, "r").read().split(' ')))
-        for components_count in (2, 5, 10):
-            grabcut(img=img, rect=rect, gmm_components=components_count)
-
-    # boundary box test
-    for img_name in boundary_box_test_images:
-        img_path = f'data/imgs/{img_name}.jpg'
-        rect_path = f'data/bboxes/{img_name}.txt'
-
-        img = cv2.imread(img_path)
-        rect = tuple(map(int, open(rect_path, "r").read().split(' ')))
-        for components_count in (2, 5, 10):
-            grabcut(img=img, rect=rect, gmm_components=components_count)
-
-    for tgt_img_name, src_images in poisson_test_images:
+    # poisson test big mask
+    for tgt_img_name, src_images in poisson_test_images_bad_mask:
         tgt_img_path = f"data/bg/{tgt_img_name}"
         im_tgt = cv2.imread(tgt_img_path, cv2.IMREAD_COLOR)
+        print(tgt_img_name)
 
         center_col = int(im_tgt.shape[1] / 2)
         center_row = int(im_tgt.shape[0] / 2)
         center = (center_col, center_row)
-        for img_name,center in src_images:
+        for img_name in src_images:
+            rect_path = f'data/bboxes/{img_name}.txt'
+            rect = tuple(map(int, open(rect_path, "r").read().split(' ')))
             im_src = cv2.imread(f"data/imgs/{img_name}.jpg", cv2.IMREAD_COLOR)
-            im_mask = cv2.imread(f"data/seg_GT/{img_name}.bmp", cv2.IMREAD_GRAYSCALE)
-            im_mask = cv2.threshold(im_mask, 0, 255, cv2.THRESH_BINARY)[1]
-            im_res = blend_images(source=im_src, target=im_tgt, center=center, mask=im_mask)
+
+            mask = np.zeros(im_src.shape[:2], dtype=np.uint8)
+            x, y, w, h = rect
+
+            # Convert from absolute coordinates
+            w -= x
+            h -= y
+
+            # Initialize the inner square to Foreground
+            mask[y:y + h, x:x + w] = 255
+
+            im_res = blend_images(source=im_src, target=im_tgt, center=center, mask=mask)
 
             print(f"saving {img_name}")
-            save_path = f"data/results/PoissonBlend/{img_name}_in_{tgt_img_name.split('.')[0]}.png"
+            save_path = f"data/results/PoissonBlend/boxy_{img_name}_in_{tgt_img_name.split('.')[0]}.png"
             cv2.imwrite(save_path, im_res)
